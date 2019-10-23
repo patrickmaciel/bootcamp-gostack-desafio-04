571973f4066268a2feb9b4fa053c26cd67c88c77
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-10-22 23:07:14 -0300

    removendo itens do estado

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -17,12 +17,23 @@ class TechList extends Component {
       newTech: '' });
   }
 
+  handleDelete = (tech) => {
+    this.setState({
+      techs: this.state.techs.filter(t => t != tech)
+    });
+  }
+
   render()  {
     return (
       <>
         <form onSubmit={this.handleSubmit}>
           <ul>
-            {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+            {this.state.techs.map(tech => (
+              <li key={tech}>
+                {tech}&nbsp;
+                <button onClick={() => this.handleDelete(tech)} type='button'>Remover</button>
+              </li>
+            ))}
           </ul>
           
           <input
