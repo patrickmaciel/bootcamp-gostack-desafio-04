40a1cb56621836088de5502c94a8a45488f88034
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-10-22 22:15:12 -0300

    Importando CSS e Imagens

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './App.css';
+
+import anime from './assets/onepunch.jpg';
+
+function App() {
+  return <img src={anime} title='Anime' />
+}
+
+export default App;
